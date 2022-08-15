eb839f8bbad1d56783095283ac61f2a4e99d7e8f
Rohit Singh Bisht <rohitsb485@gmail.com>
2022-08-15 11:43:32 +0530

    Fixed remaining models by using the created value of models if available

--- model/orderModel.ts
+++ model/orderModel.ts
@@ -81,6 +81,6 @@ const orderSchema = new mongoose.Schema(
   }
 );
 
-const Order = mongoose.model("Order", orderSchema);
+const Order = mongoose.models.Order || mongoose.model("Order", orderSchema);
 
 export default Order;
